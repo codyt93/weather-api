45bc73378211c4d8d7c169b890b247163dac4760
codyt93 <128422597+codyt93@users.noreply.github.com>
2023-05-18 09:45:37 -0700

    hwdone

--- README.md
+++ README.md
@@ -3,16 +3,12 @@
 ## Description
 this app will give you the current weather and a five day forcast .
 
-## Installation
+![screenshot](./Screenshot.png)
+
+## link
+[link](https://codyt93.github.io/weather-api/)
 
 
 ## Usage
 type the location of your choice and you are given five day weather forcast
 
-## Guidelines
-
-
-## Instructions
-
-
-## License
--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ fetch(url)
     document.getElementById("wind").innerHTML=data.wind.speed
     document.getElementById("temp").innerHTML=data.main.temp
     document.getElementById("humidity").innerHTML=data.main.humidity
+    document.getElementById("icon").src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     var url = "https://api.openweathermap.org/data/2.5/forecast?q="+input+"&appid=9f865020817046fc5bfad746c0602024&units=imperial"
 fetch(url)
 .then(res=>res.json())
@@ -18,19 +19,23 @@ fetch(url)
   console.log(data)
   for (let index = 7; index < data.list.length; index+=8) {
     const element = data.list[index];
+    const container = document.createElement("div")
    const date = document.createElement("p")
    date.innerHTML=moment.unix(element.dt).format("MMMM Do YYYY") 
-   document.getElementById("five").appendChild(date)
+   container.appendChild(date)
+   const icon = document.createElement("img")
+   icon.src = `https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`
+   container.appendChild(icon)
    const wind =document.createElement("p")
    wind.innerHTML=element.wind.speed
-   document.getElementById("five").appendChild(wind)
+   container.appendChild(wind)
    const temp = document.createElement("p")
    temp.innerHTML=element.main.temp
-   document.getElementById("five").appendChild(temp)
+  container.appendChild(temp)
    const humidity = document.createElement("p")
    temp.innerHTML=element.main.humidity
-   document.getElementById("five").appendChild(humidity)
-
+  container.appendChild(humidity)
+document.getElementById("five").appendChild(container)
   }
 })
 })
